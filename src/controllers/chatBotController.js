2037faaf36297104b426d7c84c11bdc83a09c050
Ummi Izzati <ummizati95@gmail.com>
2020-09-21 13:05:04 +0800

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-//import request from "request";
+import request from "request";
 
 let postWebhook = (req, res) =>{
     // Parse the request body from the POST
@@ -22,11 +22,11 @@ let postWebhook = (req, res) =>{
 
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
-            // if (webhook_event.message) {
-            //     handleMessage(sender_psid, webhook_event.message);
-            // } else if (webhook_event.postback) {
-            //     handlePostback(sender_psid, webhook_event.postback);
-            // }
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
 
         });
 
@@ -66,18 +66,18 @@ let getWebhook = (req, res) => {
 };
 
 // Handles messages events
-// function handleMessage(sender_psid, received_message) {
-//     let response;
-//
-//     // Check if the message contains text
-//     if (received_message.text) {
-//
-//         // Create the payload for a basic text message
-//         response = {
-//             "text": `You sent the message: "${received_message.text}". Now send me an image!`
-//         }
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
 //     } else if (received_message.attachments) {
-//
+
 //     // Gets the URL of the message attachment
 //     let attachment_url = received_message.attachments[0].payload.url;
 //         response = {
@@ -105,54 +105,54 @@ let getWebhook = (req, res) => {
 //                 }
 //             }
 //         }
-//
+
 // }
-//
-// // Sends the response message
-//     callSendAPI(sender_psid, response);
-// }
+
+// Sends the response message
+    callSendAPI(sender_psid, response);
+}
 
 // Handles messaging_postbacks events
-// function handlePostback(sender_psid, received_postback) {
-//     let response;
+function handlePostback(sender_psid, received_postback) {
+    // let response;
 
-//     // Get the payload for the postback
-//     let payload = received_postback.payload;
+    // // Get the payload for the postback
+    // let payload = received_postback.payload;
 
-//     // Set the response based on the postback payload
-//     if (payload === 'yes') {
-//         response = { "text": "Thanks!" }
-//     } else if (payload === 'no') {
-//         response = { "text": "Oops, try sending another image." }
-//     }
-//     // Send the message to acknowledge the postback
-//     callSendAPI(sender_psid, response);
-// }
+    // // Set the response based on the postback payload
+    // if (payload === 'yes') {
+    //     response = { "text": "Thanks!" }
+    // } else if (payload === 'no') {
+    //     response = { "text": "Oops, try sending another image." }
+    // }
+    // // Send the message to acknowledge the postback
+    // callSendAPI(sender_psid, response);
+}
 
 // // Sends response messages via the Send API
-// function callSendAPI(sender_psid, response) {
-//     // Construct the message body
-//     let request_body = {
-//         "recipient": {
-//             "id": sender_psid
-//         },
-//         "message": { "text": response }
-//     };
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": { "text": response }
+    };
 
-//     // Send the HTTP request to the Messenger Platform
-//     request({
-//         "uri": "https://graph.facebook.com/v7.0/me/messages",
-//         "qs": { "access_token": process.env.FB_PAGE_TOKEN },
-//         "method": "POST",
-//         "json": request_body
-//     }, (err, res, body) => {
-//         if (!err) {
-//             console.log('message sent!');
-//         } else {
-//             console.error("Unable to send message:" + err);
-//         }
-//     });
-// }
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v7.0/me/messages",
+        "qs": { "access_token": process.env.FB_PAGE_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!');
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
 
 // // function firstTrait(nlp, name) {
 // //     return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
